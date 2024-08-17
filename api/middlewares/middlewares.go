130fafb5031fca8a7897502ffdebc1f4d265bb35
muadhim <muazim2911@gmail.com>
2024-08-17 23:41:17 +0700

    [feat] handle cors

--- api/middlewares/middlewares.go
+++ api/middlewares/middlewares.go
@@ -7,20 +7,37 @@ import (
 	"net/http"
 )
 
+func SetCORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set the necessary CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		// If the request method is OPTIONS, respond and return without calling the next handler
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// For non-OPTIONS requests, proceed to the next middleware/handler
+		next.ServeHTTP(w, r)
+	})
+}
+
 func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "appllication/json")
+		w.Header().Set("Content-Type", "application/json")
 		next(w, r)
 	}
 }
 
-func SetMiddlewareAuthentication(nex http.HandlerFunc) http.HandlerFunc {
+func SetMiddlewareAuthentication(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		err := auth.TokenValid(r)
 		if err != nil {
 			responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
 			return
 		}
-		nex(w, r)
+		next(w, r)
 	}
 }
