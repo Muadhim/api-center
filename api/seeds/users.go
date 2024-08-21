c7028d14abb01b0af925c6760ce35594a9eddc47
muadhim <muazim2911@gmail.com>
2024-08-21 14:50:57 +0700

    [feat] change user sheeder item

--- api/seeds/users.go
+++ api/seeds/users.go
@@ -5,7 +5,7 @@ import "api-center/api/models"
 var Users = []models.User{
 	{
 		Name:     "ucub",
-		Email:    "ucub@gmail.com",
-		Password: "ucubPassword", // Ensure to hash password if needed
+		Email:    "ucub@api.com",
+		Password: "$2a$10$t.Qtf4t3AJDnayTqM3e90O7OrA58eDs71JUA6Atm5P8nUw.Yq9C7S", // Ensure to hash password if needed
 	},
 }
