bf5827662ecf282db6dc529874f3142d6371cacf
muadhim <muazim2911@gmail.com>
2024-09-19 20:46:01 +0700

    [feat] create project folder controller

---
+++ api/controllers/project_folder_controller.go
@@ -0,0 +1,121 @@
+package controllers
+
+import (
+	"api-center/api/auth"
+	"api-center/api/models"
+	"api-center/api/responses"
+	"api-center/utils/formaterror"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+func (s Server) CreateProjectFolder(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	projectFolder := models.ProjectFolder{}
+	err = json.Unmarshal(body, &projectFolder)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	err = projectFolder.Validate("create")
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	_, err = auth.ExtractTokenID(r)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("unauthorized"))
+		return
+	}
+
+	projectFolderCreated, err := projectFolder.SaveProjectFolder(s.DB)
+	if err != nil {
+		formattedError := formaterror.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, formattedError)
+		return
+	}
+
+	w.Header().Set("Location", fmt.Sprintf("%s%s/%d", r.Host, r.RequestURI, projectFolderCreated.ID))
+	responses.JSON(w, responses.JSONResponse{
+		Status:  http.StatusCreated,
+		Message: "Project folder successfully created",
+	})
+}
+
+func (s *Server) DeleteProjectFolder(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	id, err := strconv.ParseUint(vars["id"], 10, 64)
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+	_, err = auth.ExtractTokenID(r)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("unauthorized"))
+		return
+	}
+	projectFolder := models.ProjectFolder{}
+	projectFolder.ID = uint(id)
+
+	_, err = projectFolder.DeleteProjectFolder(s.DB)
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	w.Header().Set("Entity", fmt.Sprintf("%d", id))
+	responses.JSON(w, responses.JSONResponse{
+		Status:  http.StatusNoContent,
+		Message: "Project folder successfully deleted",
+	})
+}
+
+func (s *Server) UpdateProjectFolder(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	projectFolder := models.ProjectFolder{}
+	err = json.Unmarshal(body, &projectFolder)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	err = projectFolder.Validate("update")
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	_, err = auth.ExtractTokenID(r)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("unauthorized"))
+		return
+	}
+
+	_, err = projectFolder.UpdateProjectFolder(s.DB)
+	if err != nil {
+		formattedError := formaterror.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, formattedError)
+		return
+	}
+
+	responses.JSON(w, responses.JSONResponse{
+		Status:  http.StatusOK,
+		Message: "Project folder successfully updated",
+	})
+}
